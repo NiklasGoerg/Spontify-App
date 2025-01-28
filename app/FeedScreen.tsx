92c6013fbe79cf4dbd5f5c2a3aa6d220967dcde8
nigo711b <niklas.goerg@mailbox.tu-dresden.de>
2025-01-28 15:44:34 +0100

    grobe outlines für Posts fetchen und speichern

--- app/FeedScreen.tsx
+++ app/FeedScreen.tsx
@@ -1,36 +1,43 @@
 import React from "react";
-import { View, StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { useSelector } from "react-redux";
 import Feed from "@/components/feed/Feed";
 import { PostType } from "../types";
 import ChallengeButton from "@/components/feed/challengeButton";
 import { Link } from "expo-router"; // Importiere Link
 
-
-
 const FeedScreen = ({ navigation }: { navigation: any }) => {
   const posts: PostType[] = useSelector((state: any) => state.feed.posts);
-
 
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
         <Link href="/FriendManagementScreen" asChild>
-          <Image
-            source={require("@/assets/images/friends.svg")}
-            style={styles.icon}
-          />
+          <TouchableOpacity>
+            <Image
+              source={require("@/assets/images/friends.svg")}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
         </Link>
         <Image
           source={require("@/assets/images/logo.png")}
           style={styles.logo}
         />
-        
+
         <Link href="/ProfileScreen" asChild>
-          <Image
-            source={require("@/assets/images/profile-pic.jpg")}
-            style={styles.icon}
-          />
+          <TouchableOpacity>
+            <Image
+              source={require("@/assets/images/profile-pic.jpg")}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
         </Link>
       </View>
       <ScrollView contentContainerStyle={styles.feedContainer}>
